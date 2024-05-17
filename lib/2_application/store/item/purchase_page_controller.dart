50778af2bc37a398a363a6bdcf13ddaad4d2f5b4
hongkwon lee <pearan2@gmail.com>
2024-05-17 13:59:31 +0900

    결제 success callback 직업 보내는 페이지 만들어야함

--- lib/2_application/store/item/purchase_page_controller.dart
+++ lib/2_application/store/item/purchase_page_controller.dart
@@ -1,6 +1,8 @@
 import 'package:get/get.dart';
+import 'package:teameat/1_presentation/core/design/design_system.dart';
+import 'package:teameat/1_presentation/core/layout/snack_bar.dart';
 import 'package:teameat/2_application/core/page_controller.dart';
-import 'package:teameat/2_application/store/item/purchase_method.dart';
+import 'package:teameat/2_application/core/payment/payment_method.dart';
 import 'package:teameat/3_domain/order/i_order_repository.dart';
 import 'package:teameat/3_domain/order/order.dart';
 import 'package:teameat/3_domain/store/item/item.dart';
@@ -12,34 +14,36 @@ class PurchasePageController extends PageController {
   final Map<ItemDetail, int> items;
 
   // state
-  final _purchaseMethod = Rxn<PurchaseMethod>();
+  final _purchaseMethod = Rxn<PaymentMethod>();
 
   // getter
-  PurchaseMethod? get purchaseMethod => _purchaseMethod.value;
+  PaymentMethod? get purchaseMethod => _purchaseMethod.value;
   int get totalPrice => items.entries
       .fold(0, (prev, entry) => prev + entry.key.price * entry.value);
-  List<PurchaseMethod> get purchaseMethods => [
-        PurchaseMethod.card(),
-        PurchaseMethod.naver(),
-        PurchaseMethod.kakao(),
-        PurchaseMethod.toss()
+  List<PaymentMethod> get purchaseMethods => [
+        PaymentMethod.card(),
+        PaymentMethod.naver(),
+        PaymentMethod.kakao(),
+        PaymentMethod.toss()
       ];
 
   // setter
-  void onPurchaseMethodClick(PurchaseMethod newMethod) {
+  void onPurchaseMethodClick(PaymentMethod newMethod) {
     _purchaseMethod.value = newMethod;
   }
 
   Future<void> onPurchaseClick() async {
-    /// order 후 처리
-    react.isEventLoading = true;
-    final ret = await _orderRepo.registerOrder(RegisterOrderDto(
-        itemIdAndQuantities: items.entries
-            .map((e) => ItemIdAndQuantity(itemId: e.key.id, quantity: e.value))
-            .toList()));
-    react.isEventLoading = false;
-    print(ret);
-    // react.toPayment();
+    if (purchaseMethod == null) {
+      showError(DS.getText().pleaseSelectPaymentMethod);
+      return;
+    }
+    resolve<Order>(
+        _orderRepo.registerOrder(RegisterOrderDto(
+            itemIdAndQuantities: items.entries
+                .map((e) =>
+                    ItemIdAndQuantity(itemId: e.key.id, quantity: e.value))
+                .toList())),
+        (r) => react.toPaymentOff(r, purchaseMethod!));
   }
 
   PurchasePageController({required this.items});
